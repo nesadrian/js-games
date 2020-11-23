3505cf393b800bcde479eb99a747e3945b3ef62c
Adrian Nesvik <adrian.nesvik@lyse.net>
2020-11-23 14:06:54 +0100

    Add board and square

---
+++ src/components/SquareTTT.js
@@ -0,0 +1,6 @@
+
+export default function SquareTTT({ value }) {
+    return(
+      <section>{value}</section>
+    )
+};
--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import ScoreBoard from './ScoreBoard';
+import SquareTTT from './SquareTTT';
 
 export default function TicTacToe() {
   const [playerScore, setPlayerScore] = useState(0);
   const [cpuScore, setCpuScore] = useState(0);
+  const [board, setBoard] = useState([
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
+  ]);
 
   return (
     <section>
       <ScoreBoard playerScore={playerScore} cpuScore={cpuScore}/>
+      {board.map(square => <SquareTTT value={square}/>)}
     </section>
   )
 };
